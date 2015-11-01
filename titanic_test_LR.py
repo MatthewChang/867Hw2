dcd7184ca38873de7b749a9281b9136324f7f202
Matthew Chang <m_chang@mit.edu>
2015-11-01 18:27:13 -0500

    Added results comments and weight distrobutions

--- titanic_test_LR.py
+++ titanic_test_LR.py
@@ -78,7 +78,43 @@ print min_error
 print optimal_y
 print optimal_w
 
-print NLL(Xv,Yv,optimal_w)
-print LR_class_rate(Xv,Yv,optimal_w)
+print "Validataion NLL Error:" + str(NLL(Xv,Yv,optimal_w))
+print "Validation Error Rate:" + str(LR_class_rate(Xv,Yv,optimal_w))
+print "Optimal W vector:" + str(optimal_w)
+
+W = optimal_w
+W_norm = W/np.linalg.norm(W,1)
+print "Weight distrobution of W: " + str(np.abs(W_norm))
+
+
+'''
+Validataion NLL Error:236.657899149
+Validation Error Rate:0.206
+Optimal W vector:[[ -7.10944224e+01]
+ [ -5.24424977e+01]
+ [  1.23537314e+02]
+ [ -2.83868808e+02]
+ [  2.22966804e+00]
+ [  1.46804869e+01]
+ [ -2.07853843e+01]
+ [ -1.66356550e-01]
+ [  1.48842427e+01]
+ [ -3.33000886e+01]
+ [  1.84201380e+01]
+ [  9.81340208e-01]]
+1.0
+Weight distrobution of W: [[  1.11715047e-01]
+ [  8.24061285e-02]
+ [  1.94121795e-01]
+ [  4.46060552e-01]
+ [  3.50361480e-03]
+ [  2.30683537e-02]
+ [  3.26613553e-02]
+ [  2.61406299e-04]
+ [  2.33885278e-02]
+ [  5.23264815e-02]
+ [  2.89446981e-02]
+ [  1.54204035e-03]]
+'''
 
 
--- titanic_test_SVM.py
+++ titanic_test_SVM.py
@@ -77,7 +77,7 @@ def error_rate(X,Y,SV,b):
             cor += 1
     return (1-1.0*cor/X.shape[0])
 
-for C in np.linspace(0.01,10000,10):
+for C in np.linspace(0.5,2,20):
     SV,b = SVM(X,Y,k,C)
     rate = error_rate(Xt,Yt,SV,b)
     print rate
@@ -93,10 +93,26 @@ def compute_W(SV):
     for coeff,xi,yi in SV:
         w += coeff*xi
     return w
-
+W = compute_W(optimal_classifier[0])
 
 print "Optimal C Value:" + str(optimal_C)
-print "W vector:" + str(compute_W(optimal_classifier[0]))
-print "Best Error Rate:" + str(min_error)
+print "W vector:" + str(W)
+print "Validation Error Rate:" + str(error_rate(Xv,Yv,optimal_classifier[0],optimal_classifier[1]))
+
+W = np.matrix(W)
+W_norm = W/np.linalg.norm(W,1)
+print "Weight distrobution of W: " + str(np.abs(W_norm))
+
+#Parker: okay here are the results
+'''
+Optimal C Value:1.0
+W vector:[[  2.61652153e-06   2.57148352e-06  -5.18800505e-06   2.00000011e+00
+   -7.11311543e-09   1.62789734e-08   1.09828388e-07  -4.57625049e-11
+   -2.53898291e-06  -2.39254531e-06   4.93152822e-06]]
+Validation Error Rate:0.21
+Weight distrobution of W: [[  1.30826069e-06   1.28574169e-06   2.59400238e-06   1.00000000e+00
+    3.55655752e-09   8.13948623e-09   5.49141907e-08   2.28812512e-11
+    1.26949138e-06   1.19627259e-06   2.46576397e-06]]
+'''
     
 
